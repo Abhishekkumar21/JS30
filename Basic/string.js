506ca8ebc2a6ee6a450775caf3fea03e64d0e93e
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-10-22 10:10:36 +0000

    Added slice(),substring(), trim() methods

--- Basic/string.js
+++ Basic/string.js
@@ -64,7 +64,45 @@ if (a < b) {
   console.log(`${a} and ${b} are equal.`);
 }
 
+//Extracting a part of string
+console.log("\n-----slice()----------");
+//slice() - extracts a section of this string and returns it as a new string, without modifying the original string.
+//slice(indexStart), slice(indexStart, indexEnd)--> indexEnd will the excluded in output string, (indexEnd-1) will be taken in.
 
+const originalString = 'HelloWorld';
+
+console.log(originalString.slice(4)); //here indexEnd is not given so JS will consider indexEnd = str.length
+console.log(originalString.slice(2,5));//llo
+
+console.log(originalString.slice(-10)) //HelloWorld:  -10 = -10 + oroginalString.length = 0
+console.log(originalString.slice(-1)); //d: -1 = -1 + originalString.lenth = 9
+
+console.log(originalString.slice(-3,-1)); //rl
+
+console.log(originalString.slice(-1,-3));//''(since start>end so it will return empty string)
+
+console.log("\n------substring()-----");
+//similar to slice() but it does not accept negative index value, if given then it treats the -ve
+//values as zero(0);
+//here if start < end then values will swapped
+//returns new string, does not modify the original one
+
+console.log("\n------split()-----");
+//split() -> the splits the strings on the basics of given arguments to it and
+//returns the array of strigs generated after spliting
+
+const str3 = "Abhishek-Kumar-Yadav";
+console.log(str3.split('-')); //[ 'Abhishek', 'Kumar', 'Yadav' ]
+
+const str4 = "IamAbhishek kumar Yadav";
+console.log(str4.split(' ')); //[ 'IamAbhishek', 'kumar', 'Yadav' ]
+
+const str3Copy = str3.split();
+console.log(str3Copy); //put whole string in an array [ 'Abhishek-Kumar-Yadav' ]
+
+console.log('\n---------trim()-------------------------');
+const fNameFromform = '    Abhishek Kumar  ';
+console.log(fNameFromform.trim());
 
 
 
