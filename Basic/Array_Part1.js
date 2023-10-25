77b16e00db3dd5fe368dc9a2991bec5238b45dcd
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-10-25 18:43:32 +0000

    concat() & Spread operator

--- Basic/Array_Part1.js
+++ Basic/Array_Part1.js
@@ -1,6 +1,28 @@
 //Array- ordered colloection of  data/elements/anytype values.
 //JS array can store anytype of the value.
 
-//Creation
+//Creation:
+
 const myArray  = ['Abhihek', 23];
-console.log(myArray, typeof myArray);
+//console.log(myArray); //[ 'Abhihek', 23 ]
+//console.log(typeof myArray); //Object
+
+const anotherMyArray = new Array('Abhishek', 23);
+//console.log(anotherMyArray);    //[ 'Abhihek', 23 ]
+//console.log(typeof anotherMyArray); //Object
+
+//==============merging two or more arrays=================================================
+
+//Concat()- conct only two arrays and returns a new array
+const arr1 = ['abhishek',23];
+const arr2 = ['Kumar', 24];
+const mergedArray = arr1.concat(arr2);
+//console.log(mergedArray);
+
+//Spread operator(...) - merge/add two or more than two array and returns a new array 
+
+const bollywood_heros = ['Salman', 'Shahrukh'];
+const tollywood_heros = ['Ramcharan', 'Jr NTR'];
+
+const all_heros = [...bollywood_heros, ...tollywood_heros];
+//console.log(all_heros);
