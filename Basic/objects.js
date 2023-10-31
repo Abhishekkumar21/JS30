df36fb521dbf32bb29e16d266c6f336332cbd792
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-10-31 18:26:03 +0000

    accessing object Properties

--- Basic/objects.js
+++ Basic/objects.js
@@ -1,5 +1,25 @@
 //What is oject
 
-const userInformation = {
-    fullName: "Abhishek"
-} 
+
+//Object Creation- Object can be created by following way:
+// By Object Literal: const objetcName = { } --> this is non-singleton type
+// By Object() constructor : const ObjectName = new Object(); ----> Singleton type
+//By Object.create() 
+
+//Object literals==============================>
+
+//An empty object:
+    const object =  {}
+//object with  some assigned properties: 
+    const userInformation = {
+        firstName: "Abhishek",
+        'Last Name': "Kumar",
+        DoB: '2000-12-23',
+        isLoggedIn : true
+    } 
+
+//Access Object: object can be accessed by dot operation and also by []
+console.log(userInformation.firstName);
+console.log(userInformation["firstName"]);
+  //console.log(userInformation.last Name); // error: string type keys can be accessed only by [] method
+console.log(userInformation["Last Name"])
