345112d95ec829b419a4f2a472d1e386c36163c2
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-11-01 17:59:15 +0000

    object with Function

--- Basic/objects.js
+++ Basic/objects.js
@@ -2,9 +2,10 @@
 
 
 //Object Creation- Object can be created by following way:
-// By Object Literal: const objetcName = { } --> this is non-singleton type
-// By Object() constructor : const ObjectName = new Object(); ----> Singleton type
-//By Object.create() 
+    // By Object Literal: const objetcName = { } --> this is non-singleton type
+    // By Object() constructor : const ObjectName = new Object(); ----> Singleton type
+    //By Object.create()
+    // By Object.fromEntries() 
 
 //Object literals==============================>
 
@@ -27,7 +28,25 @@ console.log(userInformation["Last Name"])
 //adding new property to an object:
 userInformation.age = 23;
 userInformation.gender = 'M';
-console.log(userInformation);
+userInformation['Time Zone'] = 'EST',
+//console.log(userInformation);
 
 //freezing a object
 Object.freeze(userInformation);
+userInformation['country'] = 'Canada'; //this propery cannot be added in userInformation object bcoz object has been freez.
+//console.log(userInformation);
+
+
+//An object can contain any type of date even objects/NestObjects, Functions
+
+const User ={
+    fullName : 'Sam',
+    dateOfbirth : new Date('2000-12-01'),
+    calculateAge : function (){
+        let currentDate = new Date();
+        let birthDate = this.dateOfbirth;
+        return currentDate.getFullYear() - birthDate.getFullYear();
+    }
+}
+console.log(`user is ${User.fullName}.\nHis date of birth is ${User.dateOfbirth.toLocaleDateString()} and\nthe current age is ${User.calculateAge()}`);
+
