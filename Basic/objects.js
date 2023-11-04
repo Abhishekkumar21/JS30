901446f6854fb7e39e2bae58a8ac2278f1f9fc62
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-11-04 18:04:39 +0000

    Destruct EX.1

--- Basic/objects.js
+++ Basic/objects.js
@@ -176,7 +176,21 @@ const options = {
 
 
         
+//Exercise on Destucturing of object-
 
+//1):
+let user = {
+  name: "John",
+  years: 30
+};
+
+/*Write the destructuring assignment that reads:
+name property into the variable name.
+years property into the variable age.
+isAdmin property into the variable isAdmin (false, if no such property)*/
+
+let {name, years : age, isAdmin = false} = user;
+console.log(name, age, isAdmin);
 
 
 
