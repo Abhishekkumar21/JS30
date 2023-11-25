bab6238a8855931d73659055a449331956baff9d
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-11-25 18:27:51 +0000

    Nullish Coalescing & Ternary Operator

--- 02_control-flow/one.js
+++ 02_control-flow/one.js
@@ -41,3 +41,32 @@ if(loggedInFromGoogle || loggedInFromEmail) {
     console.log('User logged in')
 }
 
+
+
+
+//Nullish Coalescing Operator (??): in case of only null and undefined
+
+let val1;
+//val1 = 5 ?? 10; //5
+//val1 = null ?? 10; //10
+//val1 = undefined ?? 25; //25
+val1 = undefined ?? 10 ?? 20; //10
+//console.log(val1);
+
+
+//Ternary operator:
+
+// console.log(
+//     (10 > 15)?('true - age is greater than 15') :
+//       ('false - age is not greater than 15')
+// )
+
+let age = 17;
+
+let message = (age < 3) ? "Hi, baby" :
+    (age < 18 ) ? "Hello!" :
+    (age < 100 ) ? "Welcome!" :
+    "Not valid age"
+
+console.log(message); //Hello!
+
