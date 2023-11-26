b760336208acbed2c5ebf551201529c13cd78128
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-11-26 20:07:22 +0000

    map() array method

---
+++ 03_iterations/seven_map.js
@@ -0,0 +1,23 @@
+// Array map() method - it is a iterative method.
+/** it returns a new array with the result of a call back function for every array element.
+ *  but it does not modify/mutate the original array
+ *  basic syntax - arr.map(callbackfn:(curVal, index, array), thisArg);
+ * we can chain other methods with it very easily.
+ * 
+ */
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+//Add 10 to each number of myNumbers array-
+const newNumbers = myNumbers.map( (num) => num +10 );
+//console.log(newNumbers); //[11, 12, 13, 14, 15,16, 17, 18, 19, 20]
+
+//Method chaining with map() method ----
+
+const anotherNumbers = myNumbers.map( (num) => num * 10 )
+                                .map( (num) => num + 1 )
+                                .filter( (num) => num >=40 )
+
+console.log(anotherNumbers);
+
+
+
