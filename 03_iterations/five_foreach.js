49a11d75fb36ad0bf6f40eb3ef8aebaddb801327
Abhishek Kumar <76141244+Abhishekkumar21@users.noreply.github.com>
2023-11-26 18:48:13 +0000

    forEach() array method

--- 03_iterations/five_foreach.js
+++ 03_iterations/five_foreach.js
@@ -1,4 +1,11 @@
 
+// Array foreach() method -
+// =========================
+
+//syntax : foreach( callbackfn: (curr, index, array) )
+    // callbackfn = A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+// it does not modify the original array
+// it returns 'undefined' as returned value.
 
 const myNumbers = [1,2,3,4,5];
 
@@ -18,12 +25,12 @@ function printNum(num) {
 myNumbers.forEach( printNum );
 */
 
-                //OR
+// ==============================================================================================================================
 
 //Foreach() returns 'undefined'
 
 const returnValue = myNumbers.forEach( (num) => num);
-console.log(returnValue); //undefined
+// console.log(returnValue); //undefined
 
 
 
